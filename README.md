8c476874550342cf44abac76051311e0f80fd693
eXhumer <exhumer1@protonmail.com>
2024-01-31 16:29:45 -0700

    refactor: Add a simple websocket server
    Signed-off-by: eXhumer <exhumer1@protonmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# decky-proton-launch-rust-backend
+A [rust](https://www.rust-lang.org/) programming launguage based backend for decky-proton-launch.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use futures::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_websockets::{Error, ServerBuilder};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut server = ServerBuilder::new().accept(socket).await.unwrap();
+
+    while let Some(Ok(item)) = server.next().await {
+        println!("Received: {item:?}");
+        server.send(item).await.unwrap();
+    }
 }
